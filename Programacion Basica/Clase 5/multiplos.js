d0982178391455a2661c9c1e85081c3be94349b5
BiancaBasso <biancabassoo@hotmail.com>
2021-04-26 15:03:44 -0300

    Clase 1

--- Programacion Basica/Clase 5/multiplos.js
+++ Programacion Basica/Clase 5/multiplos.js
@@ -1,4 +1,6 @@
 let contador;
 for(contador=1; contador <=100; contador++); {
-    if(contador %2 ==0 )
-}
+    if(contador %2 ==0 || contador %3 == 0) {
+        console.log(contador,"Es multiplo de 2 o 3");
+    }
+} 
