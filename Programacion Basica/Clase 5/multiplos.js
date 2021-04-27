8e89ea2a0de1988518b7f17b915a0fa96f106b5e
BiancaBasso <biancabassoo@hotmail.com>
2021-04-27 18:45:28 -0300

    Clase 5

--- Programacion Basica/Clase 5/multiplos.js
+++ Programacion Basica/Clase 5/multiplos.js
@@ -1,6 +1,7 @@
-let contador;
-for(contador=1; contador <=100; contador++); {
-    if(contador %2 ==0 || contador %3 == 0) {
-        console.log(contador,"Es multiplo de 2 o 3");
+let readlineSync = require("readline-sync");
+let contador; 
+    for (contador =1; contador <= 100; contador++){
+        if (contador %2 == 0){
+            console.log(contador, "es multiplo de 2 && de 3")
+        }
     }
-} 
